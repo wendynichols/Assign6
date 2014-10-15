5cbcf0e9729a03457edfef1dd07be5ff63c78a18
Wendy Nichols <wendlelyn@gmail.com>
2014-10-14 21:48:18 -0400

    made changes

--- main.js
+++ main.js
@@ -6,22 +6,40 @@ console.log(i)
 //Q1 calculate the average price of all items
 
   var price = [];
-
   var push = items.forEach(function(p) {
     price.push(p.price);
-});
+  });
 
-  console.log(price);
+    console.log(price);
 
   var sum = price.reduce(function(a, b){
   return a + b;
-});
-
-  console.log(sum);
+  });
+    console.log(sum);
 
   var avg = sum / price.length;
+    console.log(avg.tofixed(2));
 
-  console.log(avg);
+
+
+
+  //CLASS REVIEW
+  //all of the prices
+    var all_prices = items.map(function (item) {
+      return item.price;
+    });
+      console.log(all_prices);
+
+    //add them all together
+    var added_together = all_prices.reduce(function(initial, current) {
+      return initial + current;
+    });
+      console.log(added_together);
+
+    var sum = added_together / items.length;
+      console.log(sum.toFixed(2));
+
+
 
 
 
@@ -30,15 +48,31 @@ console.log(i)
 
   var cost = items.filter(function(c) {
     return c.price >14.00 && c.price <18.00;
-});
+  });
+      console.log(cost);
 
-  console.log(cost)
 
+
+    //CLASS REVIEW
+    var mid_price_items = items.filter(function(item) {
+      return item.price >14.00 && item.price <18.00 && item.currency_code === 'USD';
+    });
+      console.log(mid_price_items);
+
+              //OR//
+      mid_price_items.forEach( function (item) {
+        console.log(item.title);
+      });
+
+              //OR//
+    var titles = [];
+      items.forEach(function (item) {
+      return titles.push(item.title);
+    });
+      console.log(titles);
 
 
 //Q3 find the item with a "GBP" currency code and print its name and price
-
-
 
   var cur = items.filter(function(c) {
       if ('GBP' === c.currency_code) {
@@ -48,7 +82,7 @@ console.log(i)
         }
       });
 
-    console.log(cur)
+      console.log(cur)
 
 
 
@@ -68,9 +102,8 @@ console.log(i)
 //Q5 find which items are made of eight or more materials
 
     var ml = items.filter(function(m) {
-    return m.materials <=8;
-
-  });
+      return m.materials <=8;
+    });
 
   console.log(ml)
 
